e7273702b0fc82c0764cecbaa0f23a46d8d5dcb9
Javier Gutiérrez Bravo <jgbravo.developer@gmail.com>
2022-10-29 13:22:04 +0200

    feature: searchNote use case

---
+++ core/src/commonMain/kotlin/com/jgbravo/blocappkmm/domain/note/SearchNote.kt
@@ -0,0 +1,18 @@
+package com.jgbravo.blocappkmm.domain.note
+
+import com.jgbravo.blocappkmm.domain.time.DateTimeUtil
+
+class SearchNote {
+
+    operator fun invoke(notes: List<Note>, query: String): List<Note> {
+        if (query.isBlank()) {
+            return notes
+        }
+        return notes.filter {
+            it.title.trim().lowercase().contains(query.lowercase())
+                    || it.content.trim().lowercase().contains(query.lowercase())
+        }.sortedBy {
+            DateTimeUtil.toEpochMillis(it.created)
+        }
+    }
+}
