10e95db923240dd64527203d9cdf428b9504536c
Javier Gutiérrez Bravo <jgbravo.developer@gmail.com>
2022-10-29 12:24:37 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "BlocAppKMM"
+include(":androidApp")
+include(":core")
